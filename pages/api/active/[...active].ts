3d09e2dcbb8afe26b14647ed2d3d8a8278c5f3dc
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2022-09-08 01:40:51 -0700

    image api
    changed images

--- pages/api/active/[...active].ts
+++ pages/api/active/[...active].ts
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-      res.redirect(307, "https://ipfs.io/ipfs/QmajTApCkVkXGeNotAE9XAwYDc3B8AgriFtd5jthWUUWWt/Active.gif")
+      res.redirect(307, "https://ipfs.io/ipfs/QmXJZstj5E7ZF3nWXAcdxQGbcWZAuroL5Q5DjZWq2j3FLk/")
     } catch (err) {
       res.status(500).send({ error: 'failed to fetch data' })
     }
--- pages/api/expired/[...expired].ts
+++ pages/api/expired/[...expired].ts
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-      res.redirect(307, "https://ipfs.io/ipfs/Qme5QHNUjUiZbao7Si54uveTJtUZJ3gPgVbUSJet1h4ybY/Expired.gif")
+      res.redirect(307, "https://ipfs.io/ipfs/QmXJZstj5E7ZF3nWXAcdxQGbcWZAuroL5Q5DjZWq2j3FLk/")
     } catch (err) {
       res.status(500).send({ error: 'failed to fetch data' })
     }
